2e18c2489cef2c3925ef5ae5d8ae0591c09b64e2
Madipadige-ManishKumar <oomanish459@gmail.com>
2025-09-23 19:33:14 +0530

    Time: 7 ms (6.13%) | Memory: 43.5 MB (77.13%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        HashMap <Integer,Integer> map  = new HashMap();
+        for(int i=0;i<nums2.length;i++)
+        {
+            int flag=0;
+            for(int j=i+1;j<nums2.length;j++)
+            {
+                if(nums2[i]<nums2[j] && flag ==0){
+                map.put(nums2[i],nums2[j]);
+                flag=1;
+                }
+            }
+            if(flag==0)
+            map.put(nums2[i],-1);
+        }
+        int [] res = new int[nums1.length];
+        for(int i=0;i<nums1.length;i++)
+        {
+            res[i]=map.get(nums1[i]);
+        }
+        return res;
+    }
+}
