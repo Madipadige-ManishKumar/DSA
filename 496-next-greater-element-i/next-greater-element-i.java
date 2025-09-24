079ee46c0149a146f409a4cad941f956135c536a
Madipadige-ManishKumar <oomanish459@gmail.com>
2025-09-24 05:55:40 +0530

    Time: 3 ms (89.58%) | Memory: 44.6 MB (18.92%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,20 +1,25 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        HashMap <Integer,Integer> map  = new HashMap();
-        for(int i=0;i<nums2.length;i++)
+        Stack <Integer> st = new Stack();
+        HashMap <Integer,Integer> map = new HashMap();
+        for(int i=nums2.length-1;i>=0;i--)
         {
-            int flag=0;
-            for(int j=i+1;j<nums2.length;j++)
+            /*
+             stack top and array element 
+             st > ar  push array element 
+            st < ar pop the element until you find the largest element of current array element
+            */ 
+            while((!st.isEmpty())  && (st.peek()<=nums2[i]))
             {
-                if(nums2[i]<nums2[j] && flag ==0){
-                map.put(nums2[i],nums2[j]);
-                flag=1;
-                }
+                st.pop();
             }
-            if(flag==0)
+            if(st.isEmpty())
             map.put(nums2[i],-1);
+            else
+            map.put(nums2[i],st.peek());
+            st.push(nums2[i]);
         }
-        int [] res = new int[nums1.length];
+        int res [] = new int [nums1.length];
         for(int i=0;i<nums1.length;i++)
         {
             res[i]=map.get(nums1[i]);
