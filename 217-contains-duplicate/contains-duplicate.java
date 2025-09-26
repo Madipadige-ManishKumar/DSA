b1662cc9b7167aec5433165067375950b0686567
Madipadige-ManishKumar <oomanish459@gmail.com>
2025-09-26 20:15:19 +0530

    Time: 512 ms (5.28%) | Memory: 63 MB (5.42%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i:nums)
+        {
+            if(map.containsKey(i))
+            {
+                int c=map.get(i);
+                c++;
+                map.put(i,c);
+            }
+            else
+            {
+                map.put(i,1);
+            }
+        }
+        System.out.println(map);
+        for (Map.Entry<Integer, Integer> en :map.entrySet())
+        {
+            int key=en.getKey();
+            int value =en.getValue();
+            System.out.println(value+"the value ");
+            if(value>1)
+            return true;
+        }
+        return false;
+    }
+}
