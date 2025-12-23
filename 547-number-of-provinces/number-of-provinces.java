6c237e43a4c7ec5cc5f3658fa458a134945f1b34
Madipadige-ManishKumar <oomanish459@gmail.com>
2025-12-23 14:36:49 +0530

    Time: 1 ms (85.90%) | Memory: 47.4 MB (46.70%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.java
@@ -0,0 +1,38 @@
+class Solution {
+    public void bfs(int start , int[][] adj , int []vis)
+    {
+        Queue<Integer>queue = new LinkedList<>();
+        queue.add(start);
+        vis[start]=1;
+        while(!queue.isEmpty())
+        {
+            int node = queue.poll();
+            int adjnodes []  = adj[node];
+            for(int i=0;i<adjnodes.length;i++)
+            {
+                if(adjnodes[i]==1 && vis[i]!=1){
+                    queue.add(i);
+                    vis[i]=1;
+                }
+            }
+        }
+    }
+    public int findCircleNum(int[][] isConnected) {
+        // input is [[1,1,0],[1,1,0],[0,0,1]]
+        int [] vis = new int[isConnected.length];
+        int count=0;
+        for(int i =0;i<vis.length;i++)
+        {
+            vis[i]=0;
+        }
+        for(int i=0;i<vis.length;i++)
+        {
+            if(vis[i]==0)
+            {
+                bfs(i,isConnected,vis);
+                count++;
+            }
+        }
+        return count;
+    }
+}
