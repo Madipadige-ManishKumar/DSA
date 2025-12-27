1221d25494b4c8501909369242facbaef0d9bd62
Madipadige-ManishKumar <oomanish459@gmail.com>
2025-12-27 18:36:46 +0530

    Time: 9 ms (56.37%) | Memory: 60.6 MB (46.24%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -0,0 +1,59 @@
+class Solution {
+    public void dfs(int [][]board ,int [][] vis, int r,int c , int[][] dir)
+    {
+        vis[r][c]=1;
+        int m = board.length;
+        int n = board[0].length;
+        for(int[] d: dir)
+        {
+            int nr,nc;
+            nr = r+d[0];
+            nc= c+d[1];
+            if(nr >=0 && nc>=0 && nr<m && nc<n && vis[nr][nc]==0 && board[nr][nc]==1)
+            {
+                dfs(board,vis,nr,nc,dir);
+            }
+        }
+    }
+    public int numEnclaves(int[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+
+        int [][]dir ={ {0,-1},{-1,0},{0,1},{1,0} };
+        // System.out.println(dir[0].length);
+        int [][]vis = new int [m][n];
+        for(int i=0;i<m;i++)
+        {
+            if(board[i][0] == 1)
+            {
+                dfs(board,vis,i,0,dir);
+            }
+            if(board[i][n-1]== 1)
+            {
+                dfs(board,vis,i,n-1,dir);
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+
+            if(board[0][i] == 1)
+            {
+                dfs(board,vis,0,i,dir);
+            }
+            if(board[m-1][i]== 1)
+            {
+                dfs(board,vis,m-1,i,dir);
+            }
+        }
+        int count=0;
+        for(int i =0;i<m;i++)
+        {
+            for(int j =0;j<n;j++)
+            {
+                if(board[i][j]==1 && vis[i][j]==0)
+                count++;
+            }
+        }
+        return count;
+    }
+}
