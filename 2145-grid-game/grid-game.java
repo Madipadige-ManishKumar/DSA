20a3c6e60305eb7ab85a5b9a8f42c7ccddeb03f2
Madipadige-ManishKumar <oomanish459@gmail.com>
2026-03-09 21:47:35 +0530

    Time: 4 ms (94.30%) | Memory: 96.1 MB (43.01%) - LeetSync

---
+++ 2145-grid-game/grid-game.java
@@ -0,0 +1,26 @@
+class Solution {
+    public long gridGame(int[][] grid) {
+
+        int n = grid[0].length;
+
+        long topSum = 0;
+        long bottomSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            topSum += grid[0][i];
+        }
+
+        long ans = Long.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+
+            topSum -= grid[0][i];  
+
+            ans = Math.min(ans, Math.max(topSum, bottomSum));
+
+            bottomSum += grid[1][i];
+        }
+
+        return ans;
+    }
+}
