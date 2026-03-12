86a42e5f9da00359daeeabf3aa5c90f71c3b806b
Madipadige-ManishKumar <oomanish459@gmail.com>
2026-03-12 06:32:30 +0530

    Time: 2 ms (98.88%) | Memory: 46.4 MB (76.35%) - LeetSync

--- 3540-hash-divided-string/hash-divided-string.java
+++ 3540-hash-divided-string/hash-divided-string.java
@@ -1,30 +1,20 @@
 class Solution {
 
-    public int cal_sum(String s, int start, int end, HashMap<Character,Integer> map) {
-        int sum = 0;
-
-        while(start <= end) {
-            sum += map.get(s.charAt(start));
-            start++;
-        }
-
-        return sum;
-    }
-
     public String stringHash(String s, int k) {
 
-        HashMap<Character,Integer> map = new HashMap<>();
-        String alpha = "abcdefghijklmnopqrstuvwxyz";
         StringBuilder res = new StringBuilder();
-        for(int i = 0; i < alpha.length(); i++)
-            map.put(alpha.charAt(i), i);
 
         for(int i = 0; i < s.length(); i += k) {
 
-            int sum = cal_sum(s, i, i + k - 1, map);
+            int sum = 0;
+
+            for(int j = i; j < i + k; j++) {
+                sum += s.charAt(j) - 'a';
+            }
+
             sum = sum % 26;
 
-            res.append(alpha.charAt(sum));
+            res.append((char)('a' + sum));
         }
 
         return res.toString();
