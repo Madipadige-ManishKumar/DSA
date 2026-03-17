c6f5bd2b19f1243f2c2302c0e51c32ff27f297f2
Madipadige-ManishKumar <oomanish459@gmail.com>
2026-03-17 19:16:31 +0530

    Time: 11 ms (3.57%) | Memory: 12.6 MB (42.30%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -0,0 +1,50 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def list_sort(self,head1):
+        cur11=head1
+        print("In the sort function ")
+
+        l=[]
+        while cur11:
+            l.append(cur11.val)
+            cur11=cur11.next
+        i=0
+        l=sorted(l)
+        print(l)
+        cur11=head1
+        while cur11:
+            cur11.val=l[i]
+            cur11=cur11.next
+            if(i<len(l)):
+                i=i+1
+        cur11=head1
+
+        return head1
+    
+    def mergeTwoLists(self, head1, head2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        obj1=Solution()
+        head3=ListNode()
+        cur1=head1
+        cur2=head2
+        if cur1 !=None and cur2!=None:
+            while cur1.next:
+                cur1=cur1.next
+            cur1.next=cur2
+            cur1=head1
+            cur1=obj1.list_sort(cur1)
+        elif cur1==None and cur2 !=None:
+            cur1=obj1.list_sort(cur2)
+        elif cur1!=None and cur2 ==None:
+            cur1=obj1.list_sort(cur1)
+        elif cur1==None and cur2==None: 
+            return cur1
+        return cur1
