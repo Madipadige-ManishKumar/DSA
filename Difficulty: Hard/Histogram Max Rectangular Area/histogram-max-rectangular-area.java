29eb04e37708bb8d38f0feba6662f29344ef0431
Madipadige-ManishKumar <oomanish459@gmail.com>
2026-04-14 14:32:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Histogram Max Rectangular Area/histogram-max-rectangular-area.java
@@ -0,0 +1,35 @@
+class Solution {
+    public static int getMaxArea(int arr[]) {
+        // code here
+         int n = arr.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
+                st.pop();
+            }
+            left[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+
+        st.clear();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
+                st.pop();
+            }
+            right[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            int width = right[i] - left[i] - 1;
+            max = Math.max(max, width * arr[i]);
+        }
+
+        return max;
+        
+    }
+}
